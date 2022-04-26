ba0dccd331d46376f82dc4054ec45fe1a17614e5
prkusr <prasanna.srinivasachar@colorado.edu>
2022-04-25 23:43:32 -0500

    Ummm I need practice

--- src/me/practice/Driver.java
+++ src/me/practice/Driver.java
@@ -1,13 +1,16 @@
 package me.practice;
 
 import me.practice.mothership.Code;
-import me.practice.problems.TwoSumV2;
+import me.practice.problems.LongestSubWithoutRepeatChars;
+//import me.practice.problems.TwoSumV2;
 
 public class Driver {
 
     public static void main(String[] args) {
 //      CodeTemplate<TwoSum.Input, TwoSum.Output> solver =
-        Code solver = new TwoSumV2();
+        Code solver;
+//      solver = new TwoSumV2();
+        solver = new LongestSubWithoutRepeatChars();
         solver.invoke();
     }
 }
--- src/me/practice/problems/LongestSubWithoutRepeatChars.java
+++ src/me/practice/problems/LongestSubWithoutRepeatChars.java
@@ -3,27 +3,59 @@ package me.practice.problems;
 import lombok.AllArgsConstructor;
 import me.practice.mothership.Code;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LongestSubWithoutRepeatChars extends Code {
 
+    @AllArgsConstructor
+    static class I implements Input {
+        String s;
+    }
+
+    @AllArgsConstructor
+    static class O implements Output {
+        Integer len;
+        String sample;
+    }
+
     @Override
     public List<Input> getInputs() {
-        return null;
+        List<Input> testCases = new ArrayList<>();
+        testCases.add(new I("abcabcbb")); // 3 abc
+        testCases.add(new I("bbbbb")); // 3 b
+        testCases.add(new I("pwwkew")); // 3 wke
+        testCases.add(new I("a")); // 3 wke
+        testCases.add(new I("")); // 3 wke
+        return testCases;
     }
 
     @Override
     public Output solve(Input input) {
-        return null;
+        I in = (I) input;
+        return new O(lswrc(in.s), in.s);
     }
 
     @Override
     public void print(Output output) {
-
+        O out = (O) output;
+        System.out.println("The length of longest substring for " + out.sample + " is " + out.len);
     }
 
-    @AllArgsConstructor
-    static class I implements Input {
-        String s;
+    private int lswrc(String s) {
+        char[] sample = s.toCharArray();
+
+        if (sample.length == 0 || sample.length == 1) return sample.length;
+
+        int maxLength = 0, currentIndex = 0, itrIndex = 0;
+        for (int i = 1, length = 0; i < sample.length; i++) {
+            if (sample[i - 1] != sample[i]) {
+                length++;
+            } else {
+                maxLength = Math.max(length, maxLength);
+            }
+        }
+
+        return maxLength;
     }
 }
