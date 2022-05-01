b59c1f72b16aff3ace4ae8c338e21f06053d3057
prkusr <prasanna.srinivasachar@colorado.edu>
2022-04-30 21:44:16 -0500

    Accepted

--- src/me/practice/problems/LongestSubWithoutRepeatChars.java
+++ src/me/practice/problems/LongestSubWithoutRepeatChars.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import me.practice.mothership.Code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LongestSubWithoutRepeatChars extends Code {
 
@@ -23,17 +25,19 @@ public class LongestSubWithoutRepeatChars extends Code {
     public List<Input> getInputs() {
         List<Input> testCases = new ArrayList<>();
         testCases.add(new I("abcabcbb")); // 3 abc
-        testCases.add(new I("bbbbb")); // 3 b
+        testCases.add(new I("bbbbb")); // 1 b
         testCases.add(new I("pwwkew")); // 3 wke
-        testCases.add(new I("a")); // 3 wke
-        testCases.add(new I("")); // 3 wke
+        testCases.add(new I("au")); // 2 au
+        testCases.add(new I("aa")); // 1 a
+        testCases.add(new I("")); // 0 ""
+        testCases.add(new I("dvdf")); // 3 "vdf"
         return testCases;
     }
 
     @Override
     public Output solve(Input input) {
         I in = (I) input;
-        return new O(lswrc(in.s), in.s);
+        return new O(lswrc2(in.s), in.s);
     }
 
     @Override
@@ -43,19 +47,87 @@ public class LongestSubWithoutRepeatChars extends Code {
     }
 
     private int lswrc(String s) {
+/*
+//        Older Leetcode answer
         char[] sample = s.toCharArray();
+        Map<Character, Boolean> visited = new HashMap<>();
 
         if (sample.length == 0 || sample.length == 1) return sample.length;
 
-        int maxLength = 0, currentIndex = 0, itrIndex = 0;
-        for (int i = 1, length = 0; i < sample.length; i++) {
-            if (sample[i - 1] != sample[i]) {
+        int maxLength = 0;
+        for (int i = 0, length = 0; i < sample.length; i++) {
+            if (!visited.containsKey(sample[i])) {
                 length++;
+                maxLength = Math.max(length, maxLength);
+                visited.put(sample[i], true);
             } else {
                 maxLength = Math.max(length, maxLength);
+                length = 1;
             }
         }
+        return maxLength;
+*/
 
+        char[] sample = s.toCharArray();
+        Map<Character, Integer> visited = new HashMap<>();
+
+        if (sample.length == 0 || sample.length == 1) return sample.length;
+
+        int maxLength = 0, start = 0, endIndex = 0;
+        for (int i = 0, length = 0; start < sample.length; i++) {
+//            if (!visited.containsKey(sample[i])) {
+//                length++;
+//                maxLength = Math.max(length, maxLength);
+//                visited.put(sample[i], true);
+//            } else {
+//                maxLength = Math.max(length, maxLength);
+//                length = 1;
+//                visited = new HashMap<>();
+//            }
+            char c = sample[i];
+            if (visited.containsKey(sample[i])) {
+                maxLength = Math.max(length, maxLength);
+                length = 1;
+
+                start = visited.get(c) + 1;
+                endIndex = start;
+                visited.put(sample[start], start);
+                i = start;
+            } else {
+                visited.put(c, i);
+                length++;
+                endIndex++;
+                maxLength = Math.max(length, maxLength);
+
+            }
+        }
+        return maxLength;
+    }
+
+    // Accepted answer
+    private int lswrc2(String s) {
+        char[] sample = s.toCharArray();
+        Map<Character, Integer> visited = new HashMap<>();
+
+        if (sample.length == 0 || sample.length == 1) return sample.length;
+
+        int maxLength = 0, start = 0;
+        for (int i = 0, length = 0; i < sample.length; i++) {
+            char c = sample[i];
+            if (visited.containsKey(c)) {
+                maxLength = Math.max(length, maxLength);
+                length = 1;
+
+                start = visited.get(c) + 1;
+                visited = new HashMap<>();
+                visited.put(sample[start], start); // update visited index
+                i = start;
+            } else {
+                visited.put(c, i);
+                length++;
+                maxLength = Math.max(length, maxLength); // need to decide
+            }
+        }
         return maxLength;
     }
 }
